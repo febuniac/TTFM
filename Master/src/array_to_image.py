4d89833098557fa83b429f9b606fa6eb71243c7c
febuniac <febuniac@gmail.com>
2016-09-17 14:28:15 -0300

    array_to_image
    Função que transforma um array em uma imagem

---
+++ Master/src/array_to_image.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+import matplotlib.pyplot as plt
+
+r = input('Enter the name of the image you want: ')
+
+def array_to_image(r):
+	img=mpimg.imread(r)
+	#imgplot = plt.imshow(img) #to transform again into image
+	#plt.show() #show the image
+	print(img)
+array_to_image(r)
+
+
+
+
+
+#img.shape
+#http://matplotlib.org/users/image_tutorial.html
